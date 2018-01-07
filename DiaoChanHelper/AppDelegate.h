452d44e1c586043335b0765af264cd630865a50a
CityU-QingMi <Qing.Mi@my.cityu.edu.hk>
2018-01-07 19:55:26 +0800

    Initial Commit

---
+++ DiaoChanHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiaoChanHelper
+//
+//  Created by Robert on 07/01/2018.
+//  Copyright © 2018 Sogou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiaoChanHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiaoChanHelper
+//
+//  Created by Robert on 07/01/2018.
+//  Copyright © 2018 Sogou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
